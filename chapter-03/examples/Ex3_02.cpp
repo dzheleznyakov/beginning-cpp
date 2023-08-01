f6665b6f1318ba2083f04d91e6995ace2a5febbb
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-08-01 18:31:01 +0100

    Chapter 3, Example 2

---
+++ chapter-03/examples/Ex3_02.cpp
@@ -0,0 +1,26 @@
+// Operations with enumerations
+#include <iostream>
+#include <iomanip>
+
+using std::setw;
+
+int main() 
+{
+    enum class Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
+    Day yesterday {Day::Monday}, today {Day::Tuesday}, poets_day {Day::Friday};
+
+    enum class Punctuation : char {Comma = ',', Exclamation = '!', Questions = '?'};
+    Punctuation ch = Punctuation::Comma;
+
+    std::cout << "yesterday's value is " << static_cast<int>(yesterday)
+              << static_cast<char>(ch) << " but poet's day is " << static_cast<int>(poets_day)
+              << static_cast<char>(Punctuation::Exclamation) << std::endl;
+
+    today = Day::Thursday;
+    ch = Punctuation::Questions;
+    Day tomorrow {poets_day};
+
+    std::cout << "Is today's value(" << static_cast<int>(today)
+              << ") the same as poet's day(" << static_cast<int>(poets_day)
+              << ")" << static_cast<char>(ch) << std::endl;
+}
