28379128c81f4283ac3e2b0beba68b9fef655fd1
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-08-03 07:20:50 +0100

    Update Chapter 3, Exercise 2

--- chapter-03/examples/Ex3_02.cpp
+++ chapter-03/examples/Ex3_02.cpp
@@ -7,7 +7,7 @@ using std::setw;
 int main() 
 {
     enum class Day {Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday};
-    Day yesterday {Day::Monday}, today {Day::Tuesday}, poets_day {Day::Friday};
+    Day yesterday {Day::Monday}, today {Day::Tuesday}, tomorrow {Day::Wednesday}, poets_day {Day::Friday};
 
     enum class Punctuation : char {Comma = ',', Exclamation = '!', Questions = '?'};
     Punctuation ch = Punctuation::Comma;
@@ -18,7 +18,7 @@ int main()
 
     today = Day::Thursday;
     ch = Punctuation::Questions;
-    Day tomorrow {poets_day};
+    tomorrow = {poets_day};
 
     std::cout << "Is today's value(" << static_cast<int>(today)
               << ") the same as poet's day(" << static_cast<int>(poets_day)
