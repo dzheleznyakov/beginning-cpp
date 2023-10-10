91c8dee6d2380e71c92d1db36494c146ba4b820f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2023-10-10 19:25:02 +0100

    Chapter 5, example 1

---
+++ chapter-05/examples/Ex5_01.cpp
@@ -0,0 +1,25 @@
+// Using a for-loop with an array
+#include <iostream>
+
+int main()
+{
+    const size_t size {6};
+    unsigned int height[size] {26, 37, 47, 55, 62, 75};
+    unsigned int total {};
+
+    for (size_t i {}; i < size; ++i)
+    {
+        total += height[i];
+    }
+
+    int average {static_cast<int>(total / size)};
+    std::cout << "The average hight is " << average << std::endl;
+    
+    unsigned int count {};
+    for (size_t i = 0; i < size; ++i)
+    {
+        if (height[i] < average) ++count;
+    }
+    std::cout << count << " people are below average height.";
+    
+}
