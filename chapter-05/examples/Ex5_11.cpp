56d64e2f49026bdb702f787477bf15836a193274
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-02-17 22:18:22 +0000

    Chapter 5, example 11

---
+++ chapter-05/examples/Ex5_11.cpp
@@ -0,0 +1,38 @@
+// Classifiying the letters in a string
+#include <iostream>
+#include <locale>
+
+int main()
+{
+    const int maxlength{100};
+    char text[maxlength]{};
+
+    std::cout << "Enter a line of text:" << std::endl;
+    std::cin.getline(text, maxlength);
+    std::cout << "You entered:\n"
+              << text << std::endl;
+
+    size_t vowels{};
+    size_t consonants{};
+    for (int i{}; text[i] != '\0'; ++i)
+    {
+        if (isalpha(text[i]))
+        {
+            switch (tolower(text[i]))
+            {
+            case 'a':
+            case 'e':
+            case 'o':
+            case 'i':
+            case 'u':
+                vowels++;
+                break;
+            default:
+                consonants++;
+            }
+        }
+    }
+
+    std::cout << "Your input contained " << vowels << " vowels and "
+              << consonants << " consonants." << std::endl;
+}
