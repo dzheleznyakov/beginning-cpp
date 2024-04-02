78b73ca72fe2335908f494543bf154178f828106
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-04-02 20:45:59 +0100

    Chapter 6, example 4

---
+++ chapter-06/examples/Ex6_04.cpp
@@ -0,0 +1,37 @@
+// Calculating primes using pointer notation
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+    const size_t max{100};
+    long primes[max]{2L, 3L, 5L};
+    size_t count{3};
+    long trial{5};
+    bool isprime{true};
+
+    do
+    {
+        trial += 2;
+        size_t i{};
+
+        do
+        {
+            isprime = trial % *(primes + i) > 0;
+        } while (++i < count && isprime);
+
+        if (isprime)
+        {
+            *(primes + count++) = trial;
+        }
+    } while (count < max);
+
+    std::cout << "The first " << max << " primes are:" << std::endl;
+    for (size_t i{}; i < max; ++i)
+    {
+        std::cout << std::setw(7) << *(primes + i);
+        if ((i + 1) % 10 == 0)
+            std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
