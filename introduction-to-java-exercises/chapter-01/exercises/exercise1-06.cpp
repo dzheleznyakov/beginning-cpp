a15c7a5ac1450df2f7cb845b4ec1cf1b05149328
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-04-26 21:16:28 +0100

    Intro to Java exercises: Chapter 1, exercise 6

---
+++ introduction-to-java-exercises/chapter-01/exercises/exercise1-06.cpp
@@ -0,0 +1,12 @@
+/*
+    Summation of a series:
+    Write a programs that displays the result of
+    1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10.
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = "
+              << (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10) << std::endl;
+}
