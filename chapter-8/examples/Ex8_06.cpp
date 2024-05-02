18c0090806581c29204192e5e53b3a903bd8626f
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-05-02 19:43:55 +0100

    Chaper 8, example 6

---
+++ chapter-8/examples/Ex8_06.cpp
@@ -0,0 +1,28 @@
+// Passing a two-dimensional array to a function
+#include <iostream>
+
+double yield(const double values[][4], size_t n);
+
+int main()
+{
+    double beans[3][4]{
+        {1.0, 2.0, 3.0, 4.0},
+        {5.0, 6.0, 7.0, 8.0},
+        {9.0, 10.0, 11.0, 12.0}};
+
+    std::cout << "Yield = " << yield(beans, sizeof(beans) / sizeof(beans[0]))
+              << std::endl;
+}
+
+double yield(const double array[][4], size_t size)
+{
+    double sum{};
+    for (size_t i{}; i < size; ++i)
+    {
+        for (size_t j{}; j < 4; ++j)
+        {
+            sum += array[i][j];
+        }
+    }
+    return sum;
+}
