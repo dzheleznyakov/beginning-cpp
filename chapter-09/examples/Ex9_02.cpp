c53f4f2ead8907230c3a5aefb1b0239ec2d5a535
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-06-12 18:43:46 +0100

    Chapter 9, example 2

---
+++ chapter-09/examples/Ex9_02.cpp
@@ -0,0 +1,34 @@
+// Passing lambda expressions as function arguments
+#include <iostream>
+#include <vector>
+
+template <typename F>
+std::vector<double> &change(std::vector<double> &vec, F fun)
+{
+    for (auto &x : vec)
+        x = fun(x);
+    return vec;
+}
+
+int main()
+{
+    auto cube = [](double value) -> double
+    {
+        return value * value * value;
+    };
+    auto average = [](const std::vector<double> &v) -> double
+    {
+        double sum{};
+        for (auto x : v)
+            sum += x;
+        return sum / v.size();
+    };
+
+    std::vector<double> data{1.5, 2.5, 3.5, 4.5, 5.5};
+    std::cout << "Average of data in values is " << average(data) << std::endl;
+
+    change(data, [](double x)
+           { return (x + 1.0) * (x + 2.0); });
+    std::cout << "Average of changed values in data is " << average(data) << std::endl;
+    std::cout << "Average of cubes of values in data is " << average(change(data, cube)) << std::endl;
+}
