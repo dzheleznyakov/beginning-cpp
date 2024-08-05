e1a33be5f3c5ee29840e4ea8e1913846e4e6f7be
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-08-05 07:29:11 +0100

    Chapter 10, example 5

---
+++ chapter-10/examples/Ex10_05/average.h
@@ -0,0 +1,18 @@
+#ifndef AVERAGE_H
+#define AVERAGE_H
+
+#include <type_traits>
+#include <vector>
+
+template <class T>
+T average(const std::vector<T> &values)
+{
+    static_assert(std::is_arithmetic<T>::value,
+                  "Type paramter for average() must be arithmetic.");
+    T sum{};
+    for (auto &value : values)
+        sum += value;
+    return sum / values.size();
+}
+
+#endif
