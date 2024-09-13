0933829622ff6e65db36804d4d2cdac5365b4aa7
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-09-13 07:08:15 +0100

    Chapter 14, example 2

---
+++ chapter-14/14_02/Ex14_02.cpp
@@ -0,0 +1,28 @@
+// Using virtual functions
+#include <iostream>
+#include "Box.h"
+#include "ToughPack.h"
+#include "Carton.h"
+
+int main()
+{
+    Box box{20.0, 30.0, 40.0};
+    ToughPack hardcase{20.0, 30.0, 40.0};
+    Carton carton{20.0, 30.0, 40.0, "plastic"};
+
+    box.showVolume();
+    hardcase.showVolume();
+    carton.showVolume();
+
+    Box *pBox{&box};
+    std::cout << "\nbox volume through pBox is " << pBox->volume() << std::endl;
+    pBox->showVolume();
+
+    pBox = &hardcase;
+    std::cout << "\nhardcase volume through pBox is " << pBox->volume() << std::endl;
+    pBox->showVolume();
+
+    pBox = &carton;
+    std::cout << "\ncarton volume through pBox is " << pBox->volume() << std::endl;
+    pBox->showVolume();
+}
