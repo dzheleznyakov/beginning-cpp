c657a91a93d104c63e3e99d95428c52849a7d496
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-10-16 07:36:00 +0100

    Chapter 15, example 5, different catch types

--- chapter-15/examples/Ex15_05/Ex15_05.cpp
+++ chapter-15/examples/Ex15_05/Ex15_05.cpp
@@ -25,4 +25,44 @@ int main()
         }
         std::cout << "End of the for-loop (after the catch blocks) - i is " << i << std::endl;
     }
+
+    std::cout << "\n\n"
+              << std::endl;
+    for (int i{}; i < 7; ++i)
+    {
+        try
+        {
+            if (i == 3)
+                throw trouble;
+            else if (i == 5)
+                throw moreTrouble;
+            else if (i == 6)
+                throw bigTrouble;
+        }
+        catch (const Trouble &t)
+        {
+            std::cout << typeid(t).name() << " object caught: " << t.what() << std::endl;
+        }
+        std::cout << "End of the for-loop (after the catch blocks) - i is " << i << std::endl;
+    }
+
+    std::cout << "\n\n"
+              << std::endl;
+    for (int i{}; i < 7; ++i)
+    {
+        try
+        {
+            if (i == 3)
+                throw trouble;
+            else if (i == 5)
+                throw moreTrouble;
+            else if (i == 6)
+                throw bigTrouble;
+        }
+        catch (const Trouble t)
+        {
+            std::cout << typeid(t).name() << " object caught: " << t.what() << std::endl;
+        }
+        std::cout << "End of the for-loop (after the catch blocks) - i is " << i << std::endl;
+    }
 }
