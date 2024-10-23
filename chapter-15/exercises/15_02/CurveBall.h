82bd8a04b694e0394b6a13708c202b6398a44197
dzheleznyakov <zheleznyakov.dmitry@gmail.com>
2024-10-23 07:33:38 +0100

    Chapter 15, exercise 2

---
+++ chapter-15/exercises/15_02/CurveBall.h
@@ -0,0 +1,10 @@
+#ifndef CURVE_BALL_H
+#define CURVE_BALL_H
+
+#include <stdexcept>
+
+class CurveBall : public std::exception
+{
+};
+
+#endif
---
+++ chapter-15/exercises/15_02/TooManyExceptions.h
@@ -0,0 +1,11 @@
+#ifndef TOO_MANY_EXCEPTIONS_H
+#define TOO_MANY_EXCEPTIONS_H
+
+#include <stdexcept>
+#include <string>
+
+class TooManyExceptions : public std::exception
+{
+};
+
+#endif
